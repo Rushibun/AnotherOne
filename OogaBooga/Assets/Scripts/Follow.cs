3e15a46f6107afb5d47438c6c046d99e5b5a596d
Rushibun <jessicatyu11@gmail.com>
2020-04-25 15:24:44 -0700

    [Add] Some Physics, a ball, and fixed scripts
    Raymond helped us fix up our scripts because we all no idea what we were doing, but at least we have a better idea of what to do now.

--- OogaBooga/Assets/Scripts/Follow.cs
+++ OogaBooga/Assets/Scripts/Follow.cs
@@ -7,36 +7,46 @@ public class Follow : MonoBehaviour
     
     public GameObject target;
     public float followDistance;
+    public Vector3 followOffset;
     public float cameraFollowSpeed = 0.1f;
+    public float cameraRotationSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 targetPosition = target.transform.position;
+        transform.position = CalculateCameraTargetPosition();
 
-        Vector3 followVector = transform.forward * -followDistance;
-
-        transform.position = targetPosition + followVector;
+        transform.rotation = Quaternion.LookRotation(target.transform.forward);
+        // a quaternion is an axis and an angle of rotation around that axes
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 cameraTargetPosition = CalculateCameraTargetPosition();
+
+        transform.position = Vector3.Lerp(transform.position, 
+           cameraTargetPosition, cameraFollowSpeed * Time.deltaTime);
+        // Lerp = linear interpolation = you're trying to interpolate a point btwn two points
+        // y = p1 + (p2 - p1) * t [line eqation], you're trying to find another point inbtwn those points;
+        // t = [0~1]; if t = 0, x = p1; if t = 1, x = p2; if 0 < t < 1, x = any point on line
+
+        // which way is capsule facing? --> target.transform.forward
+        Vector3 targetForward = target.transform.forward;
+        // taking camera direction and slowly rotating capsule toward
+        Vector3 direction = Vector3.RotateTowards(transform.forward, targetForward,
+           cameraRotationSpeed * Time.deltaTime, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+    Vector3 CalculateCameraTargetPosition()
+    {   
         Vector3 targetPosition = target.transform.position;
 
         Vector3 followVector = transform.forward * -followDistance;
 
-        Vector3 cameraTargetPosition = targetPosition + followVector;
+        Vector3 cameraTargetPosition = targetPosition + followVector + followOffset;
 
-        transform.position = Vector3.Lerp(transform.position, cameraTargetPosition, cameraFollowSpeed * Time.deltaTime);
-
-        Vector3 targetDir = targetPosition - transform.position;
-        float angle = Vector3.Angle(targetDir, transform.up);
-
-        if (angle < 5.0f)
-            print("close");
-        // Lerp = linear interpolation = you're trying to interpolate a point btwn two points
-        // y = p1 + (p2 - p1) * t [line eqation], you're trying to find another point inbtwn those points;
-        // t = [0~1]; if t = 0, x = p1; if t = 1, x = p2; if 0 < t < 1, x = any point on line
+        return cameraTargetPosition;
     }
 }
--- OogaBooga/Assets/Scripts/Move.cs
+++ OogaBooga/Assets/Scripts/Move.cs
@@ -5,37 +5,55 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public Vector3 speed;
-    private Vector3 target = new Vector3(5.0f, 0.0f, 0.0f);
+    
+    // amount of degrees per second you want to turn
+    public float turnspeed;
+
+    private Rigidbody rb;
+    public float jumpForce = 5.0f;
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Vector3(x,y,z) = Vector3(0.1,0.0) gives me a vector that is 0.1 units in size
-        // speed is a Vector3 
-        Vector3 currentspeed = Vector3.zero;
+    // Vector3(x,y,z) = Vector3(0.1,0.0) gives me a vector that is 0.1 units in size
+    // speed is a Vector3 
+    float currentspeed = 0.0f;
+    float currentTurnAmount = 0.0f;
 
-        if (Input.GetKey(KeyCode.A))
+    if (Input.GetKey(KeyCode.A))
     {
-        transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime);
+        currentTurnAmount -= turnspeed;
     }
     if (Input.GetKey(KeyCode.D))
     {
-        transform.RotateAround(target, Vector3.down, 30 * Time.deltaTime);
+        currentTurnAmount += turnspeed;
     }
     if (Input.GetKey(KeyCode.W))
     {
-        transform.RotateAround(target, Vector3.forward, 30 * Time.deltaTime);
+        currentspeed = speed.x;
     }
     if (Input.GetKey(KeyCode.S))
     {
-        transform.RotateAround(target, Vector3.back, 30 * Time.deltaTime);
+        currentspeed = -speed.x;
     }
-    gameObject.transform.Translate(currentspeed * Time.deltaTime);
+    gameObject.transform.Rotate(Vector3.up, currentTurnAmount * Time.deltaTime);
+    rb.AddForce(transform.forward * currentspeed * Time.deltaTime, ForceMode.Impulse);
+
+    // && is and || is or
+    if (Input.GetKeyUp(KeyCode.Space) && !isJumping)
+    {
+        isJumping = true;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+    }
+
+    rb.angularVelocity = Vector3.zero;
     }
 }
