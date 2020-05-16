9515e34542968bdb3d74ce6fa8a66c93b12df6d2
Rushibun <jessicatyu11@gmail.com>
2020-05-16 15:15:50 -0700

    [Add] Tilemapping and Some UI things
    why is it all clunky omg

--- OogaBooga/Assets/Scripts/TwoDMove.cs
+++ OogaBooga/Assets/Scripts/TwoDMove.cs
@@ -6,17 +6,22 @@ public class TwoDMove : MonoBehaviour
 {
     public float speed = 5;
     private Rigidbody2D rb;
-    public float jumpForce = 10.0f;
+    public float jumpSpeed = 3.0f;
 
     private float currentspeed = 0.0f;
     private float distancetoGround = 0.0f;
-    private int jumpCount = 0;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         distancetoGround = GetComponent<Collider2D>().bounds.extents.y;
     }
+    bool IsGrounded()
+        {
+        return Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y),Vector2.down, distancetoGround + 0.1f);
+        // return (Mathf.Abs(rb.velocity.y) < Mathf.Epsilon)
+        }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -32,22 +37,10 @@ public class TwoDMove : MonoBehaviour
         }
         rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
 
-        bool IsGrounded()
-    {
-        return Physics.Raycast(transform.position,Vector2.down, distancetoGround + 0.1f);
-        // return (Mathf.Abs(rb.velocity.y) < Mathf.Epsilon)
-    }
-        bool isGrounded = IsGrounded();
 
-        if (isGrounded)
+        if (Input.GetKeyUp(KeyCode.Space) && IsGrounded())
         {
-            jumpCount = 0;
-
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         }
     }
 }
