edef45f50551259f5d631b183116b3f9a2a52c93
Rushibun <jessicatyu11@gmail.com>
2020-05-19 17:22:12 -0700

    [Add] Pickups That Don't Work
    I need help with the trigger and making the UI work as well because idk how to use TMPro

---
+++ OogaBooga/Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+
+    }
+}
--- OogaBooga/Assets/Scripts/TwoDMove.cs
+++ OogaBooga/Assets/Scripts/TwoDMove.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TwoDMove : MonoBehaviour
 {
@@ -10,20 +12,38 @@ public class TwoDMove : MonoBehaviour
 
     private float currentspeed = 0.0f;
     private float distancetoGround = 0.0f;
+    private bool isJumping = false;
+    private LayerMask mask;
+    public float points = 0;
 
 
     void Start()
     {
+        int maskValue = LayerMask.GetMask("Player");
+        mask = ~maskValue;
+        isJumping = false;
         rb = GetComponent<Rigidbody2D>();
-        distancetoGround = GetComponent<Collider2D>().bounds.extents.y;
+        distancetoGround = GetComponent<CircleCollider2D>().radius + Mathf.Epsilon;
     }
     bool IsGrounded()
         {
-        return Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y),Vector2.down, distancetoGround + 0.1f);
-        // return (Mathf.Abs(rb.velocity.y) < Mathf.Epsilon)
+        //Cast a ray from the bottom of the player collider to 0.1 units to find a collider.  
+        //Searching in every layer but the layer that belongs to the player
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - distancetoGround), Vector2.down, 0.1f, mask);
+
+        //If the collider isn't null that means we hit something.  We are grounded.
+        return (hit.collider != null);
         }
 
     // Update is called once per frame
+    private void Update()
+    {
+        //If I hit space key and I'm grounded I'm jumping
+        if(Input.GetKeyUp(KeyCode.Space) && IsGrounded())
+        {
+            isJumping = true;
+        }
+    }
     void FixedUpdate()
     {
         float currentSpeed = 0.0f;
@@ -37,10 +57,21 @@ public class TwoDMove : MonoBehaviour
         }
         rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
 
-
-        if (Input.GetKeyUp(KeyCode.Space) && IsGrounded())
+        //If I'm jumping add the force.
+        if (isJumping)
         {
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
-        }
+            isJumping = false;   //Immediately stop jumping
     }
+    // if player touches pickup, it get points but... how to use TMPro??
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            points += 5;
+            TMPro.TMP_Text.text("Score") += 5; // what am i doing??
+        }
+
+    }
+}
 }
